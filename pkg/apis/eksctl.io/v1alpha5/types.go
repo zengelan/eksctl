7635355266c1edb0dff31edb2fe9cdabd3936ed1
Ilya Dmitrichenko <errordeveloper@gmail.com>
2019-07-30 15:06:59 +0100

    CloudWatch for cluster logging
    - add `cloudWatch.clusterLogging.enableType` config field
    - add `eksctl utils enable-logging` command
    - enable a way of calling `eks.UpdateClusterConfig` as post-creation tasks
    - enable a way to skip tasks

--- pkg/apis/eksctl.io/v1alpha5/types.go
+++ pkg/apis/eksctl.io/v1alpha5/types.go
@@ -299,13 +299,10 @@ type ClusterConfig struct {
 	// +optional
 	AvailabilityZones []string `json:"availabilityZones,omitempty"`
 
-	Status *ClusterStatus `json:"status,omitempty"`
-}
-
-// ClusterIAM holds all IAM attributes of a cluster
-type ClusterIAM struct {
 	// +optional
-	ServiceRoleARN string `json:"serviceRoleARN,omitempty"`
+	CloudWatch *ClusterCloudWatch `json:"cloudWatch,omitempty"`
+
+	Status *ClusterStatus `json:"status,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -336,6 +333,9 @@ func NewClusterConfig() *ClusterConfig {
 			Version: DefaultVersion,
 		},
 		VPC: NewClusterVPC(),
+		CloudWatch: &ClusterCloudWatch{
+			ClusterLogging: &ClusterCloudWatchLogging{},
+		},
 	}
 
 	return cfg
@@ -404,6 +404,12 @@ func (c *ClusterConfig) NewNodeGroup() *NodeGroup {
 	c.NodeGroups = append(c.NodeGroups, ng)
 
 	return ng
+}
+
+// ClusterIAM holds all IAM attributes of a cluster
+type ClusterIAM struct {
+	// +optional
+	ServiceRoleARN string `json:"serviceRoleARN,omitempty"`
 }
 
 // NodeGroup holds all configuration attributes that are
